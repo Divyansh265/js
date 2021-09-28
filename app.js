21674ec2c4951dd8a5ac732fe804440619a4bbe2
Divyansh sharma <sharmadivyansh265@gmail.com>
2021-09-28 18:56:01 +0530

    js

---
+++ app.js
@@ -0,0 +1,9 @@
+console.log(typeof"hello world")
+console.log(typeof'd')
+//number
+console.log(typeof 1)
+console.log(typeof 1.3)
+console.log(typeof -1)
+//Boolean
+console.log(typeof true)
+console.log(typeof false)

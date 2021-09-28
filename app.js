dd62df9e497d44d9f5de291c1150501727f70ca7
Divyansh sharma <sharmadivyansh265@gmail.com>
2021-09-28 19:39:34 +0530

    operator

--- app.js
+++ app.js
@@ -1,9 +1,28 @@
-console.log(typeof"hello world")
-console.log(typeof'd')
-//number
-console.log(typeof 1)
-console.log(typeof 1.3)
-console.log(typeof -1)
-//Boolean
-console.log(typeof true)
-console.log(typeof false)
+// operator
+// arthematic operator
+var a=2
+var b=4
+// console.log("a+b=",a+b)
+// console.log("a-b=",a-b)
+// console.log("a*b=",a*b)
+// console.log("a/b=",a/b)
+// console.log("a**b=",a**b)
+// console.log("a%b=",a%b)
+// assingment operator
+// b=11
+// console.log("b=",b)
+// b+=10
+// console.log("b+=10",b)
+
+// comparison operator
+
+// logical operation
+console.log("true&&false",true&&false)
+console.log("true&&true",true&&true)
+console.log("false&&false",false&&false)
+console.log("false&&true",false&&true)
+
+console.log("true||false",true||false)
+console.log("true||true",true||true)
+console.log("false||false",false||false)
+console.log("false||true",false||true)
